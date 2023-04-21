0664dce4caa1144f62f43407d173f33bebc7e954
mbingram <74675664+mbingram@users.noreply.github.com>
2023-04-21 13:18:20 -0600

    4. token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,11 +10,18 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    // Track balances
+    // Mapping is a key/value pair and a state variable
+    // We define the data type of the key and value, variable is balanceOf
+    mapping(address => uint256) public balanceOf;
+    // Send tokens
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        // msg is a Solidity global variable, sender is the address
+        // add the balance of the sender
+        balanceOf[msg.sender] = totalSupply;
     }
-
-
 }
