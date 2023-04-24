eb4b2fcb3b37f31f3154b4132cd9566e3ff09502
mbingram <74675664+mbingram@users.noreply.github.com>
2023-04-24 17:15:28 -0600

    9. Deposit tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,13 +2,43 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import './Token.sol';
 
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
 
+    // args: token address, user address, tokens to be deposited
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
+
+    // Deposit tokens
+    function depositToken(
+        address _token,
+        uint256 _amount
+    ) public {
+        // Transfer tokens to exchange (parenthesis instantiate the smart contract Token)
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+
+        // Update user balance (old balance plus transaction amount)
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+        // Emit event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    // Check balances (read from tokens mapping)
+    function balanceOf(
+        address _token,
+        address _user
+    ) public view returns (uint256) {
+        return tokens[_token][_user];
+    }
+
 }
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -83,8 +83,8 @@ contract Token {
         uint256 _value
     ) public returns (bool success) {
         // check approval
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+        require(_value <= balanceOf[_from], 'insufficient balance');
+        require(_value <= allowance[_from][msg.sender], 'insufficient allowance');
 
         // reset allowance
         allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
