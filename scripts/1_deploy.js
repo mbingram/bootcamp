8a84f6ba7643268c3d3d9f33c476e1434a4d1fdb
mbingram <74675664+mbingram@users.noreply.github.com>
2023-04-26 14:27:45 -0600

    14.1 Deployment

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,18 +1,34 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+    console.log(`Preparing deployment...\n`)
     // Fetch contract to deploy
-    const Token = await ethers.getContractFactory("Token");
+    const Token = await ethers.getContractFactory('Token');
+    const Exchange = await ethers.getContractFactory('Exchange');
+    // Fetch accounts
+    const accounts = await ethers.getSigners();
 
-    // Deploy contract
-    // write to blockchain
-    const token = await Token.deploy();
-    await token.deployed()
-    // retrieve copy once token is deployed
-    console.log(`Token Deployed to: ${token.address}`)
+    console.log(`Accounts fetched: \n${accounts[0].address}\n${accounts[1].address}\n`)
+
+    // Deploy Token contract (for MBC, mETH and mDAI)
+    const mbc = await Token.deploy('Mary Bellpepper Coin', 'MBC', '1000000')
+    await mbc.deployed()
+    console.log(`MBC Token Deployed to: ${mbc.address}`)
+
+    const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+    await mETH.deployed()
+    console.log(`mETH Token Deployed to: ${mETH.address}`)
+
+    const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+    await mDAI.deployed()
+    console.log(`mDAI Token Deployed to: ${mDAI.address}`)
+
+    const exchange = await Exchange.deploy(accounts[1].address, 10)
+    await exchange.deployed()
+    console.log(`Exchange deployed to: ${exchange.address}`)
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+    console.error(error)
+    process.exitCode = 1
 });
