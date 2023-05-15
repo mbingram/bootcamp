b21cb7381e3e1e2ca59c85bd7b4124857b00b556
mbingram <74675664+mbingram@users.noreply.github.com>
2023-05-15 16:28:55 -0600

    25. Price Chart

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ import Markets from './Markets';
 import Balance from './Balance';
 import Order from './Order';
 import OrderBook from './OrderBook';
+import PriceChart from './PriceChart';
 
 export default function App() {
   const dispatch = useDispatch()
@@ -66,7 +67,7 @@ export default function App() {
         </section>
         <section className='exchange__section--right grid'>
 
-          {/* PriceChart */}
+          <PriceChart />
 
           {/* Transactions */}
 
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect'
-import { get, groupBy, reject } from 'lodash'
+import { get, groupBy, reject, maxBy, minBy } from 'lodash'
 import { ethers } from 'ethers'
 import moment from 'moment'
 
@@ -98,7 +98,62 @@ const decorateOrderBookOrder = (order, tokens) => {
     return ({
         ...order,
         orderType,
-        orderTypeClass: ( orderType === 'buy' ? GREEN : RED ),
-        orderFillAction: ( orderType === 'buy' ? 'sell' : 'buy' )
+        orderTypeClass: (orderType === 'buy' ? GREEN : RED),
+        orderFillAction: (orderType === 'buy' ? 'sell' : 'buy')
     })
+}
+
+// PRICE CHART
+export const priceChartSelector = createSelector(
+    filledOrders,
+    tokens,
+    (orders, tokens) => {
+        if (!tokens[0] || !tokens[1]) {return null}
+        // filter orders by selected tokens
+        orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+        orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+        // sort orders by timestamp
+        orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+        // decorate orders - add display attributes
+        orders = orders.map((o) => decorateOrder(o, tokens))
+
+        // get last 2 orders for final price & price change
+        let secondLastOrder, lastOrder
+        [secondLastOrder, lastOrder] = orders.slice(orders.length - 2, orders.length)
+        // get last order price -- if it's null, pass in 0
+        const lastPrice = get(lastOrder, 'tokenPrice', 0)
+        // get second to last order price
+        const secondLastPrice = get(secondLastOrder, 'tokenPrice', 0)
+
+        return({
+            lastPrice,
+            lastPriceChange: (lastPrice >= secondLastPrice ? '+' : '-'),
+            series: [{
+                data: buildGraphData(orders)
+            }]
+        })
+    })
+
+const buildGraphData = (orders) => {
+    // group orders by hour for graph
+    orders = groupBy(orders, (o) => moment.unix(o.timestamp).startOf('hour').format())
+    // get each hour where data exists
+    const hours = Object.keys(orders)
+    // build the graph series
+    const graphData = hours.map((hour) => {
+        // fetch all orders from current hour
+        const group = orders[hour]
+        // calculate prices: open, high, low, close
+        const open = group[0] // first order
+        const high = maxBy(group, 'tokenPrice') // high price
+        const low = minBy(group, 'tokenPrice') // low price
+        const close = group[group.length - 1] // last order
+
+        return ({
+            x: new Date(hour),
+            y: [open.tokenPrice, high.tokenPrice, low.tokenPrice, close.tokenPrice]
+        })
+    })
+
+    return graphData
 }
