7b405bcf532e5d917a055717a457a23fa49567b0
mbingram <74675664+mbingram@users.noreply.github.com>
2023-06-01 11:42:38 -0600

    29. Cancel Orders

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,14 +1,20 @@
 import React, { useRef, useState } from 'react'
 import { myFilledOrdersSelector, myOpenOrdersSelector } from '../store/selectors'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import sort from '../assets/sort.svg'
 import Banner from './Banner'
+import { cancelOrder } from '../store/interactions'
 
 export default function Transactions() {
     const [showMyOrders, setShowMyOrders] = useState(true)
+    
+    const provider = useSelector(state => state.provider.connection)
+    const exchange = useSelector(state => state.exchange.contract)
     const symbols = useSelector(state => state.tokens.symbols)
     const myOpenOrders = useSelector(myOpenOrdersSelector)
     const myFilledOrders = useSelector(myFilledOrdersSelector)
+
+    const dispatch = useDispatch()
 
     const tradeRef = useRef(null)
     const orderRef = useRef(null)
@@ -23,6 +29,10 @@ export default function Transactions() {
             tradeRef.current.className = 'tab'
             setShowMyOrders(true)
         }
+    }
+
+    const cancelHandler = (order) => {
+        cancelOrder(provider, exchange, order, dispatch)
     }
 
     return (
@@ -54,7 +64,7 @@ export default function Transactions() {
                                         <tr key={index} >
                                             <td style={{ color: `${order.orderTypeClass}` }} >{order.token0Amount}</td>
                                             <td>{order.tokenPrice}</td>
-                                            <td></td>
+                                            <td><button className='button--sm' onClick={() => cancelHandler(order)} >Cancel</button></td>
                                         </tr>
                                     )
                                 })}
@@ -69,8 +79,8 @@ export default function Transactions() {
                         <h2>My Transactions</h2>
 
                         <div className='tabs'>
-                            <button onClick={tabHandler} ref={orderRef} className='tab tab--active'>Orders</button>
-                            <button onClick={tabHandler} ref={tradeRef} className='tab'>Trades</button>
+                            <button onClick={(e) => tabHandler(e)} ref={orderRef} className='tab tab--active'>Orders</button>
+                            <button onClick={(e) => tabHandler(e)} ref={tradeRef} className='tab'>Trades</button>
                         </div>
                     </div>
 
